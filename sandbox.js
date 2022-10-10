cebc5d8726b4b7c3eac27bf100a128cb48fe4c4d
Ahmed Hegazy <ahmedhegazee@hotmail.com>
2022-10-10 14:38:21 +0200

    finish lesson

---
+++ sandbox.js
@@ -0,0 +1,9 @@
+var text = "Ahmed";
+console.log(text);
+var inputs = document.querySelectorAll('input');
+console.log(inputs);
+inputs.forEach(function (input) {
+    console.log(input);
+});
+// tsc sandbox.js -w 
+//to compile and watch js files
---
+++ sandbox.ts
@@ -0,0 +1,11 @@
+const text = "Ahmed";
+console.log(text);
+const inputs = document.querySelectorAll('input');
+console.log(inputs);
+
+inputs.forEach((input) => {
+    console.log(input);
+});
+
+// tsc sandbox.js -w 
+//to compile and watch js files
