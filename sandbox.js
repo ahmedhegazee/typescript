908c522971470c5a9f9719463fa0554dc6bffadc
Ahmed Hegazy <ahmedhegazee@hotmail.com>
2022-10-10 15:28:16 +0200

    finish lesson

---
+++ sandbox.js
@@ -0,0 +1,22 @@
+var names = ['Ali', 'Ahmed', 'Khaled'];
+names.push('Salem');
+// names.push(3);//error push the same type in the array
+var numbers = [10, 45, 60];
+numbers.push(234);
+var mixed = ['fjjfjf', 10, true];
+mixed.push(89);
+//type of initialized values in the array , it will be the value pushed to array
+var ninja = {
+    name: 'Test',
+    age: 34,
+    belt: "black"
+};
+//type of properties cannot be changed later
+// ninja.age = "dds";//error
+// ninja.skills = ['sadasd','sada'];//cannot add additional property
+ninja = {
+    name: 'adas',
+    age: 0,
+    belt: "white"
+};
+//redeclare object must follow the same structure in the first initialize;
