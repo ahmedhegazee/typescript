c76c16fe543a23888587534a96c0394578897812
Ahmed Hegazy <ahmedhegazee@hotmail.com>
2022-10-12 11:33:52 +0200

    finish lesson

---
+++ sandbox.js
@@ -0,0 +1,8 @@
+var age; //means I can change the value easily
+age = 25;
+age = "ffff";
+var mixed = [];
+mixed.push('daad');
+mixed.push(12344);
+console.log(mixed);
+var ninja;
---
+++ sandbox.ts
@@ -0,0 +1,14 @@
+let age: any;//means I can change the value easily
+age = 25;
+age = "ffff";
+
+let mixed: any[]=[];
+
+mixed.push('daad');
+mixed.push(12344);
+console.log(mixed);
+
+let ninja : {
+    name: any,
+    age:any
+}
