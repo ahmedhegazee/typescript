fcb094d0b74c791b361454b296420d943a9e9e3c
Ahmed Hegazy <ahmedhegazee@hotmail.com>
2022-11-08 19:39:38 +0200

    init lesson

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,16 +1,1 @@
 "use strict";
-// let greet: Function;
-let greet;
-greet = (name, greeting) => {
-    console.log(`${name} says ${greeting}`);
-};
-let calc;
-calc = (numberOne, numberTwo, action) => {
-    if (action == "add")
-        return numberOne + numberTwo;
-    return numberOne + numberTwo;
-};
-let logDetails;
-logDetails = (human) => {
-    console.log(`${human.name} is ${human.age}`);
-};
